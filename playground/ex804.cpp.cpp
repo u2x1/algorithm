58bacc1644637adf6c83ab4d6bb5a2f5674ce265
nutr1t07 <nutr1t07@outlook.com>
2022-08-04 15:31:49 +0800

    22/8-04

--- playground/ex804.cpp.cpp
+++ playground/ex804.cpp.cpp
@@ -1,20 +1,22 @@
 #include <cstdio>
-
-
+#include <cstring>
 
 int main() {
   const int len = 10;
-  int start[len]={}, end[len]={};
+  int start[len]={}, end[len]={}, rst[len]={};
+  memset(start, 0, sizeof(start));
+  memset(end, 0, sizeof(end));
+  memset(rst, 0, sizeof(rst));
   int cnt = 0;
   int l, r;
   while(~scanf("%d%d", &l, &r)) {
     ++start[l]; ++end[r];
   }
-  int rst[len] = {};
   for(int i = 0; i < len; ++i) {
     cnt += start[i];
-    cnt -= end[i];
+    if(cnt == 0) { continue; }
     rst[i] = rst[i-1] + cnt;
+    cnt -= end[i];
   }
   for(int i = 0; i < len; ++i) {
     printf("%d ", rst[i]);
