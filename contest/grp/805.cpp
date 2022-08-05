8477f98ffd38f4f0a94cf7c4c40468c2892613e4
nutr1t07 <nutr1t07@outlook.com>
2022-08-05 12:35:28 +0800

    22/8-05

--- contest/grp/805.cpp
+++ contest/grp/805.cpp
@@ -1,14 +1,15 @@
 #include <cstdio>
 
+int dp[99999999];
+
 int main() {
-  int dp[9999999] = {1, 1, 2, 3};
+  dp[0] = 1; dp[1] = 1; dp[2] = 2; dp[3] = 3;
   for(int i = 4; i < 9999; ++i) {
     dp[i] = dp[i-4] + dp[i-1];
   }
   int x;
-  while(~scanf("%d", &x)) {
+  while(~scanf("%d", &x) && x != 0) {
     printf("%d\n", dp[x]);
   }
-
   return 0;
 }
