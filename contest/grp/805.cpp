5c9b056316288bbbb4953b94f3940069de1d62cc
nutr1t07 <nutr1t07@outlook.com>
2022-08-05 12:40:13 +0800

    22/8-05

--- contest/grp/805.cpp
+++ contest/grp/805.cpp
@@ -1,10 +1,11 @@
 #include <cstdio>
+#define LEN 999999
 
-int dp[99999999];
+int dp[LEN];
 
 int main() {
   dp[0] = 1; dp[1] = 1; dp[2] = 2; dp[3] = 3;
-  for(int i = 4; i < 9999; ++i) {
+  for(int i = 4; i < LEN; ++i) {
     dp[i] = dp[i-4] + dp[i-1];
   }
   int x;
