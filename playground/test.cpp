d0c71d1300734d9ffb7be249527c7b17fee5d221
nutr1t07 <nutr1t07@outlook.com>
2022-10-18 16:50:51 +0800

    22/10-18

--- playground/test.cpp
+++ playground/test.cpp
@@ -4,28 +4,14 @@
 #include <map>
 #include <iostream>
 using namespace std;
-map<long long,int> m;
-long long a;                                          
-int main()
-{
-	freopen("F:\\alldate\\Data0.txt","r",stdin);
-	while(scanf("%lld",&a)!=EOF){
-		auto temp = m.find(a);
-		if(temp!=m.end()){
-			*temp++;
-		}
-		else m.insert({a,1});
-		
-	} 
-	int maxn=-1;//maxn is answer
-	int maxni=0;
-	for(auto it=m.begin();it!=m.end();it++){
-		if(it->second>maxni){
-			maxni=it->second;
-			maxn=it->first;
-		}
-	}
-	fclose(stdin);
 
+long long a;                                          
+
+int main() {
+  string str;
+  while(1) {
+    getline(cin, str);
+    cout << "str: " << str << "EOF" << endl;
+  }
 	return 0;
 }
