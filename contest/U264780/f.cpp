e41c0db21971a375292ebc85f0487710ba1d2e92
u2x1 <u2x1@outlook.com>
2022-12-08 16:50:23 +0800

    22/12-08

--- contest/U264780/f.cpp
+++ contest/U264780/f.cpp
@@ -1,12 +1,34 @@
 #include <cstdio>
+#include <algorithm>
 
 #define orep(i,a,b)  for(auto i=(a); i!=(b); ++i)
 #define NL           putchar(10);
 
-const int maxN = 1e5;
-int arr[maxN];
+const int maxN = 3e5+5;
+long long arr[maxN];
+long long dp1[maxN][2], dp2[maxN][2];
 
 int main() {
-  scanf("
+  int n; scanf("%d", &n);
+  orep(i, 0, n) { scanf("%lld", arr+i); }
+  // last selected
+  dp1[0][1] = arr[0];
+  orep(i, 1, n-1) {
+    dp1[i][0] = dp1[i-1][1];
+    dp1[i][1] = std::min(dp1[i-1][0], dp1[i-1][1]) + arr[i];
+  }
+  dp1[n-1][1] = std::min(dp1[n-2][0], dp1[n-2][1]) + arr[n-1];
+  // last not selected
+  dp2[0][0] = dp2[0][1] = arr[0];
+  orep(i, 1, n-1) {
+    dp2[i][0] = dp2[i-1][1];
+    dp2[i][1] = std::min(dp2[i-1][0], dp2[i-1][1]) + arr[i];
+  }
+  dp2[n-1][0] = dp2[n-2][1];
+  // printf("%lld", dp1[n-1][1]);
+  // NL;
+  // printf("%lld", dp2[n-1][0]);
+  // NL;
+  printf("%lld", std::min(dp1[n-1][1], dp2[n-1][0]));
   return 0;
 }
